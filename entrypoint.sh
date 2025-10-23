53c5c21e7e5dc1b0878b795e9f5147d7d8399d2a
SpeedyCoder1192 <125989658+SpeedyCoder1192@users.noreply.github.com>
2025-10-23 09:12:33 +0400

    Remove API key and add signin command
    What am i doing 🤦

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,6 @@ set -e
 
 export OLLAMA_HOST="0.0.0.0"
 export OLLAMA_ORIGINS="*"
-export OLLAMA_API_KEY="768e4e73d65a418d8d9c85b74b10a9aa.IwkxumohfDNPKVKb67ZvxfiF"
 
 echo "Starting Ollama server..."
 ollama serve &
@@ -13,6 +12,9 @@ until curl -s http://localhost:11434/api/version > /dev/null; do
   echo "Waiting for Ollama to start..."
   sleep 2
 done
+
+echo "Logging in..."
+ollama signin
 
 echo "Pulling models..."
 for model in qwen3-coder:480b-cloud gpt-oss:120b-cloud gpt-oss:20b-cloud deepseek-v3.1:671b-cloud; do
